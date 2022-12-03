1d250b4ad9ce63aec31b44872f7d4fd7f460b993
ghadeer abdalhameed <gh.abd.alhameed.97@gmail.com>
2022-12-03 16:54:19 +0300

    custom fields

--- app/core/init/meta-box-init.php
+++ app/core/init/meta-box-init.php
@@ -13,15 +13,30 @@ if (count($fields) > 0):
             foreach ($fields as $key => $value):
                 add_meta_box($key, $value["name"], function ($post) use ($key, $value) {
                     wp_nonce_field('<?php echo $key . "_field_action" ?>', '<?php echo $key . "_field_name" ?>');
-                    $value_option = get_post_meta($post->ID, $key, true);
                     $output = "";
                     if ($value["type"] == "text"):
+                        $value_option = get_post_meta($post->ID, $key, true);
                         $output = <<<HTML
                         <input type="text" id="{$key}_input_id" style="width:100%;" name="{$key}_input_name" value="$value_option" />
                         HTML;
                     endif;
+                    if ($value["type"] == "image"):
+                        $value_option = get_post_meta($post->ID, $key, true);
+                        $image = $value_option ? wp_get_attachment_image_url($value_option) : app("url_plugin") . "public/assets/img/boca-img.png";
+                        $output = <<<HTML
+                            <div class="div-img-gallery">
+                                <img loading="lazy" class="img-video-poster boca-show-image" src="$image" width="100%">
+                            </div>
+                            <div class="div-button-img-poster">
+                                <a  class="btn-outline-dark tab-btn boca-btn-add-image">add</a>
+                                <a  class="btn-outline-danger tab-btn-delete boca-btn-delete-image">delete</a>
+                            </div>
+                            <input type="text" name="{$key}_input_name" id="{$key}_input_id" hidden
+                                   value="$value_option"/>
+                        HTML;
+                    endif;
                     echo $output;
-                }, $value["post_type"], 'advanced', 'default');
+                }, $value["post_type"], $value["post_type"] == "image" ? 'side' : "advanced", 'default');
             endforeach;
         });
     });
@@ -31,7 +46,12 @@ if (count($fields) > 0):
                 return;
             }
             foreach ($fields as $key => $value):
+                if($value["type"] == "text"):
                 update_post_meta($post_id, $key, sanitize_text_field($_POST["{$key}_input_name"]));
+                endif;
+                if($value["type"] == "image"):
+                    update_post_meta($post_id, $key, absint($_POST["{$key}_input_name"]));
+                endif;
             endforeach;
         });
     });
---
+++ app/public/assets/js/main.js
@@ -0,0 +1,20 @@
+let boca_btn_add_image = document.querySelectorAll(".boca-btn-add-image");
+let boca_btn_delete_image = document.querySelectorAll(".boca-btn-delete-image");
+if(boca_btn_add_image != null){
+    boca_btn_add_image.forEach(el=>{
+        el.addEventListener("click",e =>{
+            wp.media({
+                title: 'Select image',
+                button: {
+                    text: 'use this image',
+                },
+                multiple: false,
+            }).open().on("select",function (){
+                let id = this.models[0].attributes.selection.models[0].attributes.id;
+                let url = this.models[0].attributes.selection.models[0].attributes.url;
+                e.target.parentElement.previousElementSibling.firstElementChild.setAttribute("src" , url);
+                e.target.parentElement.nextElementSibling.setAttribute("value" , id);
+            })
+        })
+    })
+}
