d1d2eb5c4ebbb092bc37915ca8e8d97427a416dd
ghadeer abdalhameed <gh.abd.alhameed.97@gmail.com>
2022-12-06 10:57:54 +0300

    custom fields edit and create gallery fields taxonomy

--- app/public/function.php
+++ app/public/function.php
@@ -8,6 +8,6 @@ Hooks::Init("admin_enqueue_scripts", function () {
 		wp_enqueue_media();
 		wp_enqueue_style('boca-bootstrap-css', plugin_dir_url(__FILE__) . '/assets/css/bootstrap.min.css', array(), '1.0.0');
 		wp_enqueue_style('boca-main-css', plugin_dir_url(__FILE__) . '/assets/css/main.css', array(), '1.0.0');
-		wp_enqueue_script("boca-main-js", plugin_dir_url(__FILE__) . '/assets/js/main.js', array('jquery', 'media-upload'), '1.0.0', true);
+		wp_enqueue_script("boca-main-js", plugin_dir_url(__FILE__) . '/assets/js/boca.min.js', array('jquery', 'media-upload'), '1.0.0', true);
 	});
 });
